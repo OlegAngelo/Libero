6235cef036054bc72a0b0e18ebf2f698afa55ecd
OlegAngelo <naubalagtas@gmail.com>
2020-03-18 22:28:32 +0800

    added jquery validation for submit form

---
+++ dist/js/main.js
@@ -0,0 +1,50 @@
+$(function() {
+
+	$.validator.setDefaults({
+	
+		errorClass: 'help-block',
+		highlight: function(element) {
+			$(element)
+			.closest('.form-group')
+			.addClass('has-error');
+		},
+		unhighlight: function (element) {
+			$(element)
+			.closest('form-group')
+			.removeClass('has-error');
+		}
+	
+	});
+
+
+	$("#submit-form").validate({
+		rules: {
+			postcode: {
+				required: true,
+				digits: true,
+			},
+			email: {
+		 		required: true,
+				email: true
+			},
+			mobileNumber: {
+				required: true,
+				digits: true
+			}
+		},
+		messages: {
+			postcode: {
+				digits: 'Please enter a <em>valid</em> postal code'
+			},
+			email: {
+				email: 'Please enter a <em>valid</em> email address'
+			},
+			mobileNumber: {
+				digits: 'Please enter a <em>valid</em> phone number'
+			}
+		},
+  		submitHandler: function(form) {
+    			form.submit();
+  		}
+	});
+});
