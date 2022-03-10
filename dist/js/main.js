3afba8d248c34093e3b2c902affec2c533708158
OlegAngelo <angelonino1999@gmail.com>
2022-03-10 21:55:00 +0800

    check

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,35 +1,27 @@
-$(function() {
-
+$(function () {
 	$.validator.setDefaults({
-	
-		errorClass: 'help-block',
-		highlight: function(element) {
-			$(element)
-			.closest('.form-group')
-			.addClass('has-error');
+		errorClass: "help-block",
+		highlight: function (element) {
+			$(element).closest(".form-group").addClass("has-error");
 		},
 		unhighlight: function (element) {
-			$(element)
-			.closest('form-group')
-			.removeClass('has-error');
-		}
-	
+			$(element).closest("form-group").removeClass("has-error");
+		},
 	});
 
-	$(document).ready(function() {
-		$(".submit-btn").click(function() {
+	$(document).ready(function () {
+		$(".submit-btn").click(function () {
 			var selectedChoice = $("input[name='yesNoBtn']:checked").val();
 
-			if(!selectedChoice){
-				document.getElementById("choice").innerHTML= "Select a value.";
+			if (!selectedChoice) {
+				document.getElementById("choice").innerHTML = "Select a value.";
 			}
 		});
 	});
 
-	$(document).ready(function() {
-		$('.search-btn').click(function() {
-			$('.search-container').toggleClass('active')
-		
+	$(document).ready(function () {
+		$(".search-btn").click(function () {
+			$(".search-container").toggleClass("active");
 		});
 	});
 
@@ -40,27 +32,27 @@ $(function() {
 				digits: true,
 			},
 			email: {
-		 		required: true,
-				email: true
+				required: true,
+				email: true,
 			},
 			mobileNumber: {
 				required: true,
-				digits: true
-			}
+				digits: true,
+			},
 		},
 		messages: {
 			postcode: {
-				digits: 'Please enter a <em>valid</em> postal code'
+				digits: "Please enter a <em>valid</em> postal code",
 			},
 			email: {
-				email: 'Please enter a <em>valid</em> email address'
+				email: "Please enter a <em>valid</em> email address",
 			},
 			mobileNumber: {
-				digits: 'Please enter a <em>valid</em> phone number'
-			}
+				digits: "Please enter a <em>valid</em> phone number",
+			},
 		},
-  		submitHandler: function(form) {
-    			form.submit();
-  		}
+		submitHandler: function (form) {
+			form.submit();
+		},
 	});
 });
